fd2aa8d8a392938ba4f6f33fae4ed3fd37a474a6
Diego Rebora <bogacore@hotmail.com>
2023-10-02 09:06:45 -0300

    Trabajo terminado

--- src/components/Nav/navigation.js
+++ src/components/Nav/navigation.js
@@ -9,7 +9,8 @@ export default function Nav({itemMenu}) {
     <header className="bg-blue">
       <nav className="navbar navbar-expand-lg w-100 bg-blue">
         <div className="container-fluid justify-content-between align-items-center">
-            <Link to="/" className="text-decoration-none" >
+            <Link to="/" className="text-decoration-none" 
+            >
             <h1 className="navbar-brand cursor-p">Rick & Morty</h1>
             </Link>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
